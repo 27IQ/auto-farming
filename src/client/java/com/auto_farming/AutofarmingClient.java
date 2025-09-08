c0d58e417146d06c2f138a92a1121f06eb401b13
-27IQ <83514344+27IQ@users.noreply.github.com>
2025-09-08 23:57:02 +0200

    inti commit

---
+++ src/client/java/com/auto_farming/AutofarmingClient.java
@@ -0,0 +1,10 @@
+package com.auto_farming;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutofarmingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
